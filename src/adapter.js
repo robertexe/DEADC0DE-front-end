3dc3220419ba8a124a7072584be0fe273d196dbd
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-31 16:15:15 -0400

    initial commit

---
+++ src/adapter.js
@@ -0,0 +1,34 @@
+// // DELETE
+// fetch(`http://localhost:3000/projects/#{id}`, {
+// 	method: 'DELETE',
+// 	headers: {
+// 		"Content-Type": "application/json",
+// 		"Accepts": "application/json"
+// 	}})
+//
+// // CREATE
+// fetch('http://localhost:3000/projects', {
+// 	method: 'POST',
+// 	headers: {
+// 		"Content-Type": "application/json",
+// 		"Accepts": "application/json"
+// 	},
+// 	body: JSON.stringify({
+// 		name: 'fake',
+//     description: 'fake',
+//     url: 'fake.com',
+//     language_id: 18
+//   })
+// })
+//
+// fetch('http://localhost:3000/users', {
+// 	method: 'POST',
+// 	headers: {
+// 		"Content-Type": "application/json",
+// 		"Accepts": "application/json"
+// 	},
+// 	body: JSON.stringify({
+// 		name: "mark",
+// 		experience_level: 1
+//   })
+// })
