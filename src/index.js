50c3f21bed32ef82bc66649a7232acff462262ab
mark sauer-utley <mark.utley.713@gmail.com>
2018-08-01 15:40:23 -0400

    give navbar dynamic position

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ document.addEventListener('DOMContentLoaded', init)
 
 
 
-
-
 function init() {
+  const navBar = document.querySelector('.navigation-bar')
   const languageList = document.querySelector('#language-list')
-  const languageCardContainer = document.querySelector('language-card-container')
-  Adapter.get('languages').then(json => renderLanguages(json.data))
+  const languageCardContainer = document.querySelector('#language-card-container')
+
+  window.addEventListener('scroll', handleNavBarScroll)
+
+  Adapter.get('languages')
+    .then(json => {
+      // renderLanguages(json.data)
+      renderLanguageCards(json.data)
+    })
 
 
+  function handleNavBarScroll(e) {
+    if (window.pageYOffset >= 170) {
+      navBar.style.position = "fixed"
+    } else {
+      navBar.style.position = "sticky"
+    }
+  }
 
   function makeLanguageTemplate(language) {
     let attributes = language.attributes
@@ -26,14 +39,21 @@ function init() {
   }
 
   function makeLanguageCard(language) {
-
+    let attributes = language.attributes
+    return `<div class="parent">
+              <div class="child bg-1">
+                <img src="${attributes.icon}">
+                <a href="#" data-language_id=${language.id}>${attributes.name}</a>
+              </div>
+            </div>`
   }
 
   function makeLanguageCards(languages) {
-
+    return languages.map(language => makeLanguageCard(language)).join('')
   }
 
   function renderLanguageCards(languages) {
-
+    let template = makeLanguageCards(languages)
+    languageCardContainer.innerHTML += template
   }
 }
