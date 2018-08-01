39a0cc147b2d309ff78733b1b82cbe0137323ff8
robertexe <robtabares92@gmail.com>
2018-08-01 12:14:29 -0400

    added sidebar and navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', init)
 
+
+
+
+
 function init() {
-  
+  const languageList = document.querySelector('#language-list')
+  Adapter.get('languages').then(json => renderLanguages(json.data))
+
+
+
+  function makeLanguageTemplate(language){
+    let attributes = language.attributes
+    return  `<li data-language_id="${language.id}" data-icon="${attributes.icon}">${attributes.name}(${attributes.projectcount}) </li>`
+  }
+
+  function makeLanguageTemplates(languages){
+    return languages.map(language => makeLanguageTemplate(language)).join('')
+  }
+
+  function renderLanguages(languages){
+    let template = makeLanguageTemplates(languages)
+    languageList.innerHTML += template
+  }
 }
