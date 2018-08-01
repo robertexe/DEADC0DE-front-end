d43a66b27e6838fe96223b659d3d7d178e32e282
mark sauer-utley <mark.utley.713@gmail.com>
2018-08-01 14:15:33 -0400

    fix stickiness

--- src/index.js
+++ src/index.js
@@ -6,21 +6,34 @@ document.addEventListener('DOMContentLoaded', init)
 
 function init() {
   const languageList = document.querySelector('#language-list')
+  const languageCardContainer = document.querySelector('language-card-container')
   Adapter.get('languages').then(json => renderLanguages(json.data))
 
 
 
-  function makeLanguageTemplate(language){
+  function makeLanguageTemplate(language) {
     let attributes = language.attributes
     return  `<li data-language_id="${language.id}" data-icon="${attributes.icon}">${attributes.name}(${attributes.projectcount}) </li>`
   }
 
-  function makeLanguageTemplates(languages){
+  function makeLanguageTemplates(languages) {
     return languages.map(language => makeLanguageTemplate(language)).join('')
   }
 
-  function renderLanguages(languages){
+  function renderLanguages(languages) {
     let template = makeLanguageTemplates(languages)
     languageList.innerHTML += template
+  }
+
+  function makeLanguageCard(language) {
+
+  }
+
+  function makeLanguageCards(languages) {
+
+  }
+
+  function renderLanguageCards(languages) {
+
   }
 }
